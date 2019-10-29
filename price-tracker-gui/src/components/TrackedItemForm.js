044febd3743fa049f67fa7e28f1a5f476d7b3370
884170 <james.q.quick@fedex.com>
2019-10-29 14:55:47 -0500

    Stubbed out tracked item for in react and now requiring 'bearer' in header for token

---
+++ price-tracker-gui/src/components/TrackedItemForm.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import axios from "axios";
+
+export default function TrackedItemForm(props) {
+
+    const [phone, setPhone] = React.useState(props.phone);
+    const [price, setPrice] = React.useState(0);
+    const [url, setUrl] = React.useState('');
+
+    const reset = () => {
+        setPrice(0);
+        setUrl('');
+    }
+
+    const submitTrackedItem = async (e) => {
+        e.preventDefault();
+        console.log(phone, price, url);
+        const trackedItem = { phone, price, url };
+        try {
+            const res = await axios.post('/api/trackedItem', trackedItem)
+            //TODO: feedback to user on success
+            reset();
+        } catch (err) {
+            console.err(err)
+        }
+
+    }
+
+
+    return (
+        <div>
+            <form onSubmit="submitTrackedItem">
+                <label for="phone">Phone Number</label>
+                <input type="text" placeolder="1-555-555-5555" value={phone}
+                    onChange={(e) => setPhone(e.target.value)}></input>
+
+                <label for="price">Target Price</label>
+                <input type="number" step="0.01" value={price} min="0"
+                    onChange={(e) => setPrice(e.target.value)}></input>
+
+                <label for="url">Amazon URL</label>
+                <input type="text" placeolder="www.amazon.com/abc" value={url}
+                    onChange={(e) => setUrl(e.target.value)}></input>
+                <button type="submit">Track Item</button>
+            </form>
+        </div>
+    )
+}
